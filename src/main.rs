2a212940b718682aebed91bb4065a71e138900f3
George Hester <48884343+georgehester@users.noreply.github.com>
2024-01-28 15:38:14 +0000

    refactor: cleaned up option menu code added comments to help

--- src/main.rs
+++ src/main.rs
@@ -16,54 +16,52 @@ fn main()
             .collect::<Vec<String>>(),
         0,
     );
-
-    println!();
 }
 
 fn show_menu(options: Vec<String>, default: usize)
 {
-    let mut stdout = std::io::stdout();
+    // Get the current data from the input
     let mut current: usize = default;
     let length: usize = options.len();
 
+    // Get stdout
+    let mut stdout = std::io::stdout();
+
+    // Set terminal to raw mode
     crossterm::terminal::enable_raw_mode().unwrap();
 
-    //let window_size: WindowSize = crossterm::terminal::window_size().unwrap();
+    // Calculate if we need more space to show output
+    let scroll: i32 = (crossterm::cursor::position().unwrap().1 as i32)
+        - (crossterm::terminal::window_size().unwrap().rows as i32)
+        + (length as i32);
 
-    //stdout.execute(crossterm::cursor::MoveToColumn(0)).unwrap();
-
-    //let cursor_position: (u16, u16) = crossterm::cursor::position().unwrap();
-
-    //println!("{:?}", crossterm::cursor::position().unwrap());
-
-    let cursor_position = crossterm::cursor::position().unwrap();
-    let window_size = crossterm::terminal::window_size().unwrap();
-    let extra_lines: i32 = (cursor_position.1 as i32) - (window_size.rows as i32) + (length as i32);
-
-    if extra_lines > 0
+    // Scroll if we need more space
+    if scroll > 0
     {
         stdout
-            .execute(crossterm::terminal::ScrollUp(extra_lines as u16))
+            .execute(crossterm::terminal::ScrollUp(scroll as u16))
             .unwrap()
             .execute(crossterm::cursor::MoveTo(
                 0,
-                cursor_position.1 - (extra_lines as u16),
+                crossterm::cursor::position().unwrap().1 - (scroll as u16),
             ))
             .unwrap();
     }
 
+    // Hide the cursor and save its current position
     stdout
-        .execute(crossterm::cursor::Show)
+        .execute(crossterm::cursor::Hide)
         .unwrap()
         .execute(crossterm::cursor::SavePosition)
         .unwrap();
 
+    // Render loop
     loop
     {
-        let reset_position = stdout.execute(crossterm::cursor::SavePosition).unwrap();
-
+        // Output the options to the terminal
         for (index, option) in options.iter().enumerate()
         {
+            // Highlight the current option
             if index == current
             {
                 stdout
@@ -72,14 +70,7 @@ fn show_menu(options: Vec<String>, default: usize)
                         crossterm::cursor::position().unwrap().1,
                     ))
                     .unwrap()
-                    .execute(crossterm::style::Print(format!(
-                        " > {} {}",
-                        option,
-                        crossterm::cursor::position().unwrap().1,
-                        //crossterm::cursor::position().unwrap().1 + (index as u16),
-                        //window_size.rows,
-                        //index
-                    )))
+                    .execute(crossterm::style::Print(format!(" > {}", option)))
                     .unwrap();
             }
             else
@@ -90,17 +81,11 @@ fn show_menu(options: Vec<String>, default: usize)
                         crossterm::cursor::position().unwrap().1,
                     ))
                     .unwrap()
-                    .execute(crossterm::style::Print(format!(
-                        "   {} {}",
-                        option,
-                        crossterm::cursor::position().unwrap().1,
-                        //crossterm::cursor::position().unwrap().1 + (index as u16),
-                        //window_size.rows,
-                        //index
-                    )))
+                    .execute(crossterm::style::Print(format!("   {}", option)))
                     .unwrap();
             }
 
+            // Move cursor down a line if required
             if index < length
             {
                 stdout
@@ -111,8 +96,7 @@ fn show_menu(options: Vec<String>, default: usize)
             }
         }
 
-        //println!("{:?}", crossterm::cursor::position().unwrap());
-
+        // Poll for user input
         if crossterm::event::poll(std::time::Duration::from_millis(100)).unwrap()
         {
             match crossterm::event::read().unwrap()
@@ -143,6 +127,7 @@ fn show_menu(options: Vec<String>, default: usize)
             }
         }
 
+        // Restore cursor position and clear the current output
         stdout
             .execute(crossterm::cursor::RestorePosition)
             .unwrap()
@@ -152,6 +137,10 @@ fn show_menu(options: Vec<String>, default: usize)
             .unwrap();
     }
 
+    // Reset cursor view and disable raw mode
     stdout.execute(crossterm::cursor::Show).unwrap();
     crossterm::terminal::disable_raw_mode().unwrap();
+
+    // Print terminating byte
+    println!();
 }
